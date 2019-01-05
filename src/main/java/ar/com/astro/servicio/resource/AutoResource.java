94946857f33ce63b555c8c59bce6970f8013f38d
Astro <astroatmoky@hot.com>
2019-01-05 13:13:29 -0300

    cambio

--- src/main/java/ar/com/astro/servicio/resource/AutoResource.java
+++ src/main/java/ar/com/astro/servicio/resource/AutoResource.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AutoResource {
 
 
-    //Cuando envien un metodo GET al servicio se invoca este servicio
+    //Cuando envien una paticion por Http GET al servicio se invoca este servicio
     @GetMapping(path ="car/{id}",produces= "application/json")
     public AutoResponse getAutoById(@PathVariable(name ="id") Long id) {
 
